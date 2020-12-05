f9f27d0cd2a9523ccf8abdd29ded1f8b9a013a85
wahid-d <wakhid2802@gmail.com>
2020-12-05 18:28:47 +0900

    Home layout finished

---
+++ spa/Models/Post.cs
@@ -0,0 +1,13 @@
+namespace spa.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+
+        public string Subtitle { get; set; }
+
+        public string HtmlContent { get; set; }
+
+        public string Tags { get; set; }
+    }
+}
